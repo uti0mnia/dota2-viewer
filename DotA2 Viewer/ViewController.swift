aabdbce80e94485609d61009874f2a183511df69
Casey McLewin <mclewinc@gmail.com>
2016-10-06 15:49:46 -0400

    Initial Commit

---
+++ DotA2 Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DotA2 Viewer
+//
+//  Created by McLewin, Casey on 2016-10-06.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
