204ae3fb2fc7ddbd0b5fdc2229a4504ba3f9b15c
casey <uti0mnia@gmail.com>
2016-12-11 22:34:20 -0500

    Started Abilities SV

--- DotA2 Viewer/Views/AbilitiesStackView.swift
+++ DotA2 Viewer/Views/AbilitiesStackView.swift
@@ -10,5 +10,6 @@ import UIKit
 
 class AbilitiesStackView: UIStackView {
     
+    var subStackViews: [AbilityView]
 
 }
---
+++ DotA2 Viewer/Views/AbilityStackView.swift
@@ -0,0 +1,23 @@
+//
+//  AbilityStackView.swift
+//  DotA2 Assistant
+//
+//  Created by Casey McLewin on 2016-12-11.
+//  Copyright © 2016 self. All rights reserved.
+//
+
+import UIKit
+
+class AbilityStackView: UIStackView {
+    // MARK - Properties
+    var nameLabel: DAMainLabel!
+    var abilityImageView: UIImageView!
+    var cooldownLabel: DAMainLabel!
+    var manaLabel: DAMainLabel!
+    var typesLabel: DAMainLabel!
+    var summaryLabel: DAMainLabel!
+    var dataLabel: DAMainLabel!
+    var modifiersLabel: DAMainLabel!
+    
+
+}
