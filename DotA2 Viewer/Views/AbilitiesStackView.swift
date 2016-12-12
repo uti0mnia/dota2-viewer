42f1b8fe6caaabbe7c84e897de2f43e4f8ed0713
Casey McLewin <=>
2016-12-12 13:48:52 -0500

    Finished AbilitiesStackView

--- DotA2 Viewer/Views/AbilitiesStackView.swift
+++ DotA2 Viewer/Views/AbilitiesStackView.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class AbilitiesStackView: UIStackView {
+    // MARK - Properties
+    fileprivate var topLabel: DAMainLabel = {
+        let label = DAMainLabel(style: .large)
+        label.text = ""
+        label.sizeToFit()
+        label.textAlignment = .center
+        return label
+    }()
+    fileprivate var subStackViews = [AbilityStackView]()
     
-    var subStackViews: [AbilityView]
+    
+    // MARK - Initializers
+    init(frame: CGRect = CGRect(), count: Int) {
+        super.init(frame: frame)
+        
+        // create <count> expandable stack views and add them
+        for _ in 0..<count {
+            let sv = AbilityStackView()
+            subStackViews.append(sv)
+        }
+        self.addArrangedSubviews(views: subStackViews)
+        
+        // setup
+        setup()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK - Methods
+    fileprivate func setup() {
+        self.axis = .vertical
+        self.alignment = .fill
+        self.distribution = .fillProportionally
+        self.spacing = 8
+    }
 
 }
