c9d314dcdd58ba6e6b2dd5b9cf7ee98186825366
casey <uti0mnia@gmail.com>
2016-12-12 20:10:18 -0500

    Added KVO to remove the DATextExpandableStackView from the view if it has no text.

--- DotA2 Viewer/Views/DAExpandableTextStackView.swift
+++ DotA2 Viewer/Views/DAExpandableTextStackView.swift
@@ -11,7 +11,6 @@ import UIKit
 class DAExpandableTextStackView: DAExpandableStackView {
     // MARK - Properties
     var textLabel: DAMultiLineLabel!
-    
     // MARK - Initializaers
     init(name: String, frame: CGRect = CGRect()) {
         // set the main label
@@ -23,12 +22,26 @@ class DAExpandableTextStackView: DAExpandableStackView {
         // set the text label
         textLabel = DAMultiLineLabel(style: .small)
         super.init(topView: topLabel, subView: textLabel, expanded: false, frame: frame)
+        
+        // observer to follow the changes in the text label
+        textLabel.addObserver(self, forKeyPath: "text", options: [.new], context: nil)
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        textLabel.removeObserver(self, forKeyPath: "text")
+    }
     
 
+    // MARK - Methods
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "text" {
+            if textLabel.text == "" {
+                self.removeFromSuperview()
+            }
+        }
+    }
 }
