26e95a0d527164470b56ba1fa0288cfb7527903b
Casey McLewin <mclewinc@gmail.com>
2017-06-12 22:31:03 -0700

    Added ability controller

---
+++ DotA2 Viewer/AbilityTVC.swift
@@ -0,0 +1,73 @@
+//
+//  AbilityTVC.swift
+//  DotA2 Assistant
+//
+//  Created by Casey McLewin on 2017-06-10.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+class AbilityTVC: UIViewController, UITableViewDataSource {
+    
+    private static let abilityCellIndetifier = "abilityCell"
+    private static let estimatedRowHeight: CGFloat = 200
+    
+    private var tableView: UITableView!
+    
+    public var abilities: [Ability]!
+    
+    init(abilities: [Ability]) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.abilities = abilities
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        assert(abilities != nil, "Abilties must be set for AbilityTVC")
+        
+        // tableview setup
+        tableView = UITableView(frame: self.view.bounds)
+        tableView.register(AbilityCell.self, forCellReuseIdentifier: AbilityTVC.abilityCellIndetifier)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = AbilityTVC.estimatedRowHeight
+    }
+    
+    private func configure(_ cell: AbilityCell, at indexPath: IndexPath) {
+        let ability = abilities[indexPath.row]
+        
+        cell.nameLabel.text = ability.name
+        cell.specialsLabel.attributedText = ability.specialIcons
+        cell.abilityImageView.image = UIImage(named: ability.imageName)
+        
+        cell.abilityStackView.types = ability.types?.array as? [ModifiableValue]
+        cell.abilityStackView.abilityDescription = ability.descrip
+        cell.abilityStackView.data = ability.data?.array as? [ModifiableValue]
+        cell.abilityStackView.modifiers = ability.modifiers?.allObjects as? [Modifier]
+        cell.abilityStackView.cooldown = ability.cooldown
+        cell.abilityStackView.mana = ability.mana
+        cell.abilityStackView.notes = ability.notes?.array as? [Note]
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return abilities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: AbilityTVC.abilityCellIndetifier, for: indexPath) as! AbilityCell
+        configure(cell, at: indexPath)
+        return cell
+    }
+
+}
