68e49cdab9f68f4492f35445b861a07420eb6d45
Casey McLewin <mclewinc@gmail.com>
2017-06-13 20:44:50 -0700

    Added Stretchy Header. Removed SwiftyJSON

--- Pods/Target Support Files/Pods-DotA2 Assistant/Pods-DotA2 Assistant-frameworks.sh
+++ Pods/Target Support Files/Pods-DotA2 Assistant/Pods-DotA2 Assistant-frameworks.sh
@@ -85,13 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GSKStretchyHeaderView/GSKStretchyHeaderView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GSKStretchyHeaderView/GSKStretchyHeaderView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
