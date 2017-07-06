af1cb56003b5ee50486ab0d690aafe05a8e9fd2a
Casey McLewin <mclewinc@gmail.com>
2017-07-05 22:43:32 -0700

    Fixed attribute size in HeroAttributeView

--- DotA2 Viewer/HeroAttributeView.swift
+++ DotA2 Viewer/HeroAttributeView.swift
@@ -29,7 +29,7 @@ class HeroAttributeView: UIView {
         
         sv.alignment = .fill
         sv.axis = .horizontal
-        sv.distribution = .equalSpacing
+        sv.distribution = .fillEqually
         
         return sv
     }()
